9f5a8b29ddf8561a3e9332f01c31291d0677b6b2
Shivain Thapar <shivain27@gmail.com>
2018-07-12 15:45:27 -0400

    Show posts from the api requests on root page

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,13 @@
-// action creator
+// actions file
 
 import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
 
-export function fetchPosts() {
+export function fetchPosts() { // action creator
 
-    const ROOT_URL = 'http://reduxblog.herokuapp.com/api/';
-    const API_KEY = '?key=2';
+    const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
+    const API_KEY = '?key=27';
     const req = axios.get(`${ROOT_URL}/posts${API_KEY}`);
 
     return {
