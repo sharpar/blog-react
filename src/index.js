a8f156c38385b9c4d747f7c344a05c04db4762ae
Shivain Thapar <shivain27@gmail.com>
2018-07-12 16:35:32 -0400

    Create new component for new post, use Switch to fix react route bug
    - /posts/new was still rendering the posts-index component because it
    was matching all routes with a /

--- src/index.js
+++ src/index.js
@@ -2,20 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import reducers from './reducers';
 import PostsIndex from './components/pos-index';
 import promise from 'redux-promise';
+import PostsNew from './components/posts-new';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    {/*<App />*/}
-    <BrowserRouter>
-        <div>
-            <Route path={'/'} component={PostsIndex}/>
-        </div>
-    </BrowserRouter>
-  </Provider>
-  , document.querySelector('.container'));
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        {/*<App />*/}
+        <BrowserRouter>
+            <div>
+                <Switch>
+                    <Route path={'/posts/new'} component={PostsNew}/>
+                    <Route path={'/'} component={PostsIndex}/>
+                </Switch>
+            </div>
+        </BrowserRouter>
+    </Provider>
+    , document.querySelector('.container')
+);
