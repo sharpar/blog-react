3f0cffccc7f138284958288b099e83947f888991
Shivain Thapar <shivain27@gmail.com>
2018-07-17 17:10:18 -0400

    Added a delete button

--- src/components/posts-show.js
+++ src/components/posts-show.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { fetchPost } from '../actions';
+import { deletePost, fetchPost } from '../actions';
 
 class PostsShow extends Component {
 
     componentDidMount() { // React lifecylce event, runs automatically on component render
-        const { id } = this.props.match.params;
+        const { id } = this.props.match.params; // params object provided by react router 
         this.props.fetchPost(id);
+    }
+
+    onDeleteClick() {
+        const { id } = this.props.match.params;
+        this.props.deletePost(id, () => {
+            this.props.history.push('/');
+        });
     }
 
     render() {
@@ -18,6 +25,11 @@ class PostsShow extends Component {
         return (
             <div>
                 <Link to={'/'}>Back to index</Link>
+                <button
+                    className="btn btn-danger pull-xs-right"
+                    onClick={this.onDeleteClick.bind(this)}>
+                    Delete Post
+                </button>
                 <h3>{post.title}</h3>
                 <h6>Categories: {post.categories}</h6>
                 <p>{post.content}</p>
@@ -31,4 +43,4 @@ function mapStateToProps({ posts }, ownProps) {
     return { post: posts[ownProps.match.params.id] };
 }
 
-export default connect(mapStateToProps, { fetchPost })(PostsShow);
+export default connect(mapStateToProps, { deletePost, fetchPost })(PostsShow);
--- src/reducers/reducer-posts.js
+++ src/reducers/reducer-posts.js
@@ -1,11 +1,13 @@
 import _ from 'lodash';
-import { FETCH_POST, FETCH_POSTS } from '../actions'
+import { DELETE_POST, FETCH_POST, FETCH_POSTS } from '../actions'
 
 
 export default function (state = {}, action) {
     switch (action.type) {
+        case DELETE_POST:
+            return _.omit(state, action.payload); // deletes key of id if it exists, returns a new state object  
         case FETCH_POST:
-            return { ...state, [action.payload.data.id]: action.payload.data }; // add fetched post to state 
+            return { ...state, [action.payload.data.id]: action.payload.data }; // add fetched post to state
         case FETCH_POSTS:
             return _.mapKeys(action.payload.data, 'id');
         default:
